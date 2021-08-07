fefec4774550a06364a590a93c12e605c89bf687
fatou267 <67634804+fatou267@users.noreply.github.com>
2021-08-07 15:20:03 +0000

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+define('username', '3548671_bambasocio');
+define('password', ')WH9Ninh8p.CIjZU');
+define('host', 'fdb28.awardspace.net');
+define('database', '3548671_bambasocio');
+
+
+
+
+
+ ?>
