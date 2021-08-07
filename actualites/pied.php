3f789d3a8f00c364762a3eac679f3831637129b8
fatou267 <67634804+fatou267@users.noreply.github.com>
2021-08-07 15:16:18 +0000

    Add files via upload

---
+++ actualites/pied.php
@@ -0,0 +1,50 @@
+
+</div>
+</div>
+</div>
+</section>
+</div>
+<br><br><br>
+
+<div class="footer ">
+  <div class="footer-content">
+    <div class="footer-section about">
+      <h1>BSS</h1>
+<p>Bamba Socio Services est là pour aidre ,orienter et encourager les étudiants. Le jeune Baol Baol est là pour soutenir les jeunes baol baol donc n'héritez pas <strong id="h1">BSS</strong> est là pour vous donc profitez<img src="http://bambasocio.atwebpages.com/src/image/smiley_smile.gif" ></p>
+  <div class="contact">
+    <span><i class="phone"><img alt="mail" src="http://bambasocio.atwebpages.com/src/image/tel.png"  width="5%" height="5%" title="Appelez nous">&nbsp; 78-131-21-24</i></span><br>
+    <br>
+    <span><i class="enveloppe"><img alt="mail" src="http://bambasocio.atwebpages.com/src/image/gmail.png"  width="5%" height="5%" title="Constatez nous via gmail">Bambasocio@gmail.com</i></span>
+  </div>
+  <br>
+  <div class="social">
+<a ><img alt="facebook" src="http://bambasocio.atwebpages.com/src/image/facebook.png" width="5%" height="5%" title="Rejoignez nous sur facebook"></a>
+<a><img alt="twitter" src="http://bambasocio.atwebpages.com/src/image/twitter.png"  width="5%" height="5%" title="Rejoignez nous sur Twitter"></a>
+<a><img alt="instagram" src="http://bambasocio.atwebpages.com/src/image/instagram.jpg" width="5%" height="5%" title="Rejoignez nous sur Instagram"></a>
+  </div>
+    </div>
+    <div class="footer-section links">
+      <h2>Informations</h2>
+      <ul>
+        <li><a href="http://bambasocio.atwebpages.com/src/aide.php" class="perso">Cas social</a></li>
+        <li><a href="http://bambasocio.atwebpages.com/src/conditions.php" class="perso">Conditions d'utilisation</a></li>
+        <li><a href="http://bambasocio.atwebpages.com/src/propos.php" class="perso">A propos</a></li>
+        <li><a href="http://bambasocio.atwebpages.com/etudiants"  class="perso">Nouveaux bacheliers</a></li>
+      </ul>
+    </div>
+    <div class="footer-section contact">
+      <h2>Contactez nous</h2>
+      <p>Vous pouvez contactez directement Bamba socio par mail </p>
+        <a href="mailto:Bambasocio@gmail.com"><button>Your mail.....</button> </a>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    copyright &copy; Bamba Socio Services | Design by niangbalo 
+  </div> 
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
